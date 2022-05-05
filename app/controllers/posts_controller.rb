932a059aa66e1142ed0fbabf6eca09733ef64e1a
zozors <romance.dawn1989@gmail.com>
2022-05-05 18:03:10 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,6 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(content: params[:content])
-    redirect_to action: :index
     render json:{ post: post }
   end
 end
